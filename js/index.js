103a873bc76e24f10ece3470e940a80f92577c34
jslee971221 <88294804+jslee971221@users.noreply.github.com>
2022-04-12 18:00:28 +0900

    접근성, 태블릿

--- js/index.js
+++ js/index.js
@@ -7,7 +7,8 @@ $(function(){
     var cnt01Index;
     var cnt04Btn = $('.cnt04 .btns i');
     var cnt04artGroup = $('.cnt04 .artGroup');
-    var cnt04Index;
+    var ftrBtn = $('footer nav li:nth-child(1)');
+    var wd = $(window).width;
 
     bars.click(function(){
         if(hdrFlag){
@@ -43,6 +44,19 @@ $(function(){
             cnt04artGroup.css({marginLeft: '-100%'}); 
         });
     });
-
-
+    
+    //tablet footer
+    if(wd > 480 && wd <= 1024){
+        ftrBtn.click(function(){
+            var th = $(this);
+            var i = th.parent().index();
+            if($(this).find('i').attr('class') == 'xi-caret-down'){
+                $('footer ul').eq(i).css('height', '360px'); 
+                th.find('i').attr('class','xi-caret-up');
+            }else{
+                $('footer ul').eq(i).css('height', '40px'); 
+                th.find('i').attr('class','xi-caret-down');
+            }
+        });
+    }
 });
